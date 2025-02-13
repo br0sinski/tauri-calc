8e66232f70d49c9988379725b9aeb945ad63d32f
br0sinski <bastian.rosinski@hotmail.com>
2025-02-13 18:44:22 +0100

    fix: API version mismatch - add: Greet Test

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { invoke } from "@tauri-apps/api/core";
+
+    let name = '';
+    let msg = '';
+    
+    async function greet() {
+        try {
+            msg = await invoke('greet', { name });
+            console.log("Greeted!");
+        } catch (err) {
+            console.error("Failed to greet", err);
+            msg = "Failed to communicate with backend."
+        }
+    }
+</script>
+
+
+<div>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name}/>
+    <button on:click="{greet}">Greet!</button>
+    <p>{msg}</p>
+</div>
